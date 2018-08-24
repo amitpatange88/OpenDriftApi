070cf778ea62f4560aaeef50f5a07c760c28cbe0
Amit Patange <amitpatange88@gmail.com>
2018-08-24 21:36:16 +0530

    added new DateTimeController for all types of datetime operations

---
+++ Controllers/DateTimeController.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OpenDriftApi.Controllers
+{
+    /// <summary>
+    /// This controller helps to manage all types of datetime operations.
+    /// Here first we are getting all timezone dates based on specified datetime.
+    /// </summary>
+    [RoutePrefix("api/datetime")]
+    public class DateTimeController : ApiController
+    {
+        /// <summary>
+        /// Get all timezone list here
+        /// </summary>
+        /// <returns></returns>
+        [Route("timezones")]
+        [HttpGet]
+        public HttpResponseMessage GetTimeZoneList()
+        {
+            List<TimeZone> timeZones = new List<TimeZone>();
+            var localtime = DateTime.Now;
+            foreach (TimeZoneInfo z in TimeZoneInfo.GetSystemTimeZones())
+            {
+                var dataTimeByZoneId = TimeZoneInfo.ConvertTime(localtime, TimeZoneInfo.Local, TimeZoneInfo.FindSystemTimeZoneById(z.Id.ToString()));
+                timeZones.Add(new TimeZone { ID = z.Id, DisplayName = z.DisplayName, BaseUtcOffset = z.BaseUtcOffset.ToString(), TIMESTAMP = dataTimeByZoneId.ToString() });
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(timeZones));
+        }
+    }
+
+    /// <summary>
+    /// TimeZone class
+    /// </summary>
+    public class TimeZone
+    {
+        [JsonProperty("ID")]
+        public string ID { get; set; }
+
+        [JsonProperty("DisplayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("UTCOffset")]
+        public string BaseUtcOffset { get; set; }
+
+        [JsonProperty("Timestamp")]
+        public string TIMESTAMP { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 <b>OpenDriftApi :</b><br>
 
-It contains useful public apis which is been regualrly used in different project areas. I will list down all types of upcoming api which in will include regualrly basis on demand. 
+It contains useful public apis which is been regualrly used in different project areas. I will list down all types of upcoming api which in will include regualrly basis on demand. <br>
+
+Controller i will mention here and there usage. <br>
+     
+<ul>
+  <li> DateTimeController :
+  		<ol>
+  			<li>
+  				Get : api/datetime/timezones/ = This will get all timezones and there time based on current datetime.	
+  			</li>
+  		</ol>
+  </li>
+</ul>
