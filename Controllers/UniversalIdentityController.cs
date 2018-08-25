974a9eb329a3d88d7024d228929b52324ba28588
Amit Patange <amitpatange88@gmail.com>
2018-08-25 17:34:52 +0530

    universal unique identity controller added

---
+++ Controllers/UniversalIdentityController.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OpenDriftApi.Controllers
+{
+    /// <summary>
+    /// This controller helps to manage all types of universal identity operations.
+    /// </summary>
+    [RoutePrefix("api/unniversalidentity")]
+    public class UniversalIdentityController : ApiController
+    {
+        /// <summary>
+        /// Get universal IDENTITY as unique id here
+        /// </summary>
+        /// <returns></returns>
+        [Route("uuid")]
+        [HttpGet]
+        public HttpResponseMessage GetUniversalUniqueIdentityNumber()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(Guid.NewGuid().ToString()));
+        }
+    }
+}
